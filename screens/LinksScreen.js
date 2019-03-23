82e08a6d79bc1fc27d8f001aa1a8ede6acc9d345
calebrigg <calebrigg97@gmail.com>
2019-03-22 17:15:39 -0700

    Added the barcode scanner and barcode type differentiation.

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,27 +1,48 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
-import { ExpoLinksView } from '@expo/samples';
+import { StyleSheet, Text, View } from 'react-native';
+import { BarCodeScanner, Permissions } from 'expo';
 
-export default class LinksScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Links',
-  };
+export default class BarcodeScannerExample extends React.Component {
+  state = {
+    hasCameraPermission: null,
+  }
+
+  async componentDidMount() {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({ hasCameraPermission: status === 'granted' });
+    }
 
   render() {
+    const { hasCameraPermission } = this.state;
+
+    if (hasCameraPermission === null) {
+      return <Text>Requesting for camera permission</Text>;
+    }
+    if (hasCameraPermission === false) {
+      return <Text>No access to camera</Text>;
+    }
     return (
-      <ScrollView style={styles.container}>
-        {/* Go ahead and delete ExpoLinksView and replace it with your
-           * content, we just wanted to provide you with some helpful links */}
-        <ExpoLinksView />
-      </ScrollView>
+      <View style={{ flex: 1 }}>
+        <BarCodeScanner
+          onBarCodeScanned={this.handleBarCodeScanned}
+          style={StyleSheet.absoluteFill}
+        />
+      </View>
     );
   }
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 15,
-    backgroundColor: '#fff',
-  },
-});
+  handleBarCodeScanned = ({ type, data }) => {
+
+    if (type==512){alert(data);}
+    else if (type==32){alert(data);}
+    else{
+    //alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+    alert("Barcode format is not supported");
+    }
+  }
+
+ // function barcodeToUPC (){
+
+ // }
+
+}
