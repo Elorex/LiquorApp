f78db4605d06798e5c4d4e1d06ebac1355c7770f
Mitchell David A Calder <mitchelldavidacalder@Mitchells-MacBook-Pro.local>
2019-03-22 21:57:37 -0700

    Pray with me boys

--- screens/BarcodeScreen.js
+++ screens/BarcodeScreen.js
@@ -2,6 +2,20 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { BarCodeScanner, Permissions } from 'expo';
 
+function barcodeToUPC (data){
+  var UPC = data.slice(1,data.length);
+  alert(UPC);
+}
+
+function barcodeToEAN (data){
+  var EAN = data.slice(2,data.length);
+  alert(EAN);
+}
+
+function addToLib(bcode){
+  //Insert into SHELF the data from the Library, where the UPC matches the entered data.
+}
+
 export default class BarcodeScannerExample extends React.Component {
   state = {
     hasCameraPermission: null,
@@ -30,19 +44,13 @@ export default class BarcodeScannerExample extends React.Component {
       </View>
     );
   }
-
   handleBarCodeScanned = ({ type, data }) => {
 
-    if (type==512){alert(data);}
-    else if (type==32){alert(data);}
+    if (type==512){barcodeToUPC(data);}
+    else if (type==32){barcodeToEAN(data);}
     else{
     //alert(`Bar code with type ${type} and data ${data} has been scanned!`);
     alert("Barcode format is not supported");
     }
   }
-
- // function barcodeToUPC (){
-
- // }
-
 }
