884fbc334fb51278d93434d19a03a74acd761bfa
Kashmoney1419 <a.davesar@hotmail.com>
2019-03-23 07:33:58 -0700

    a whole lotta shit

--- screens/BarcodeScreen.js
+++ screens/BarcodeScreen.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { BarCodeScanner, Permissions } from 'expo';
+import { BarCodeScanner, Permissions, SQLite } from 'expo';
+import { BaseModel, types } from 'expo-sqlite-orm';
 
-export default class BarcodeScannerExample extends React.Component {
+var db = SQLite.openDatabase('LiquorAppTest.db')
+
+export default class BarcodeScanner extends React.Component {
   state = {
     hasCameraPermission: null,
   }
@@ -16,10 +19,10 @@ export default class BarcodeScannerExample extends React.Component {
     const { hasCameraPermission } = this.state;
 
     if (hasCameraPermission === null) {
-      return <Text>Requesting for camera permission</Text>;
+      return <Text style={styles.text}>Requesting for camera permission</Text>;
     }
     if (hasCameraPermission === false) {
-      return <Text>No access to camera</Text>;
+      return <Text style={styles.text}>No access to camera</Text>;
     }
     return (
       <View style={{ flex: 1 }}>
@@ -31,18 +34,32 @@ export default class BarcodeScannerExample extends React.Component {
     );
   }
 
-  handleBarCodeScanned = ({ type, data }) => {
+  handleBarcodeScanned = ({ type, data }) => {
+    var liquorType;
+    if (type==512) {
+      db.transaction ((tx) => {
+        tx.executeSql('SELECT type FROM ingredient where upc=?', [data], (tx, results) => {
+          var len = results.rows.length;
+          if (len > 0) {
+            liquorType = results.rows.items(0).type;
+          } else {
+            alert('UPC does not exist');
+            return;
+          }
+        });
+        tx.executeSql('INSERT into useringredient(user, ingredient) values (?, ?)', ['user', liquorType], (tx));
+        alert('Added to Shelf');
+      });
 
-    if (type==512){alert(data);}
-    else if (type==32){alert(data);}
-    else{
-    //alert(`Bar code with type ${type} and data ${data} has been scanned!`);
-    alert("Barcode format is not supported");
-    }
+   } else {alert("Barcode format is not supported");}
   }
-
- // function barcodeToUPC (){
-
- // }
-
 }
+
+const styles = StyleSheet.create({
+  text: {
+    flex: 1,
+    fontSize: 50,
+    textAlign: 'center',
+    paddingTop: 200,
+  },
+});
